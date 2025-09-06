87b6180d5da8264bdc81f073c47c06d426abede3
loglinn05 <loglinn05@gmail.com>
2025-09-06 17:52:49 +0300

    Modified the API startup script to remove setting APP_URL

--- run-app.sh
+++ run-app.sh
@@ -1,18 +1,5 @@
 #!/bin/sh
 
-# Dynamically set APP_URL if it's available from Render
-if [ -z "$APP_URL" ]; then
-    if [ ! -z "$RENDER_EXTERNAL_URL" ]; then
-        printf "🌐 APP_URL not set, using RENDER_EXTERNAL_URL \n"
-        export APP_URL="$RENDER_EXTERNAL_URL"
-    else
-        printf "🌐 APP_URL not set, using localhost \n"
-        export APP_URL="http://localhost:8000"
-    fi
-else
-    printf "🌐 APP_URL is already set \n"
-fi
-
 printf "⚙️ Running Laravel setup commands... \n"
 php artisan migrate --force
 php artisan db:seed --force
